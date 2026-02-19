829b1c8a1c626a09373d8bfe358e77ecd272742f
KNL-32 <k3laura2@gmail.com>
2026-02-19 20:14:17 +0200

    Update storage.js

--- student-finance-tracker/scripts/storage.js
+++ student-finance-tracker/scripts/storage.js
@@ -1,12 +1,22 @@
-const KEY = "finance:data";
+const STORAGE = "transactionArr";
 
-export function loadData() {
-  return JSON.parse(localStorage.getItem(KEY) || "[]");
+// Save transactions to localStorage
+export function saveTransactions(transactionArr) {
+  const transactionsString = JSON.stringify(transactionArr);
+  localStorage.setItem(STORAGE, transactionsString);
 }
 
-export function saveData(data) {
-  localStorage.setItem(KEY, JSON.stringify(data));
+// Load transactions from localStorage
+export function loadTransactions() {
+  const savedData = localStorage.getItem(STORAGE);
+
+  if (savedData) {
+    return JSON.parse(savedData);
+  } else {
+    return [];
+  }
 }
+
 
 ///loadData() - retrieves the stored financial records from localStorage
 ///saveData(data) - takes an array of financial records and saves it to localStorage 
